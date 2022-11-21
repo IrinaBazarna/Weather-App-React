e4c013def726837e44581bb0286b453e3e77479a
IrinaBazarna <108300701+IrinaBazarna@users.noreply.github.com>
2022-11-21 21:41:05 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Weather from "./Weather";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Wheather App</h1>
+      <Weather />
+      <p>
+        <a href="">Open sourse code</a> by Irina Bazarna
+      </p>
+    </div>
+  );
+}
